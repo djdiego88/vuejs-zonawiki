6537c3bfdcc4466740fe6d5da896e33e7055477b
Diego Castillo <djdiego88@gmail.com>
2019-05-13 22:38:34 -0500

    Taller 2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Vue.js Zonawiki"/>
+    <!--<HelloWorld msg="Vue.js Zonawiki"/>-->
+    <Taller1 quiubole="Que hubo pues" @el-evento="muestreMsg"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Taller1 from './components/Taller1.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Taller1
+  },
+  methods: {
+    muestreMsg() {
+      alert('Alerta emitida desde el componente hijo')
+    }
   }
 }
 </script>
---
+++ src/components/Taller1.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+      <input type="text" v-model="nombre">
+      <p>{{quiubole}} {{nombre}}!</p>
+      <a @click="hiceClic('Esto sale cuando hago clic')">Haz clic aquí</a>
+      <h3>Computed properties</h3>
+      <p>{{ nombreAlReves }}</p>
+      <h3>Watcher</h3>
+      <input type="text" v-model="texto">
+      <small>{{ ayuda }}</small>
+      <h3>Filtros</h3>
+      {{ nombre | capitalize }}
+      <h3>Eventos</h3>
+      <button @click="$emit('el-evento')">Emite evento al padre</button>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Taller1',
+        props: {
+            quiubole: String
+        },
+        data() {
+            return {
+                nombre: '',
+                texto: '',
+                ayuda: ''
+            }
+        },
+        methods: {
+            hiceClic(text) {
+                alert(text)
+            }
+        },
+        computed: {
+            // a computed getter
+            nombreAlReves: function () {
+              // `this` points to the vm instance
+              return this.nombre.split('').reverse().join('')
+            }
+        },
+        watch: {
+            // whenever question changes, this function will run
+            texto: function (nuevoTexto, viejoTexto) {
+              this.ayuda = 'El campo de texto acabó de cambiar'
+            }
+        },
+        filters: {
+            capitalize: function (value) {
+                if (!value) return ''
+                value = value.toString()
+                return value.toUpperCase()
+            }
+        }
+    }
+</script>
