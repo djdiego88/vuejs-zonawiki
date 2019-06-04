8e7c99d3a7ae5045708c24156fd3a2689b0dda95
Diego Castillo <djdiego88@gmail.com>
2019-06-03 21:18:51 -0500

    Uploaded Taller 3

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!--<Taller1 quiubole="Que hubo pues" @clic-color="showMsg"/>-->
-    <navigation></navigation>
-    <router-view quiubole="Que hubo pues" @clic-color="showMsg"></router-view>
+    <Taller1 quiubole="Que hubo pues" @clic-color="showMsg"/>
+    <!--<navigation></navigation>
+    <router-view quiubole="Que hubo pues" @clic-color="showMsg"></router-view>-->
     <div v-if="showDiv">Se ha cambiado el color del mensaje del usuario</div>
   </div>
 </template>
 
 <script>
-//import Taller1 from './components/Taller1.vue'
-import Navigation from './components/Navigation.vue'
+import Taller1 from './components/Taller1.vue'
+//import Navigation from './components/Navigation.vue'
 
 export default {
   name: 'app',
   components: {
-    //Taller1,
-    Navigation
+    Taller1,
+    //Navigation
   },
   data() {
     return {
